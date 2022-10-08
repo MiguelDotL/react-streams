8ecc77ae3ddf72d4ed44f40f60cbcf3dad4e33f4
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-08 18:05:30 -0400

    add redux boilerplate: placeholder reducer, createStore, Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-render(<App />, document.getElementById("root"));
+const store = createStore(reducers);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
