67ac88eea538676f4b1e932e1adeb5bd2c0f9e27
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-10 09:42:20 -0400

    add userId to stream state in createStream action

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,9 +6,46 @@ class StreamList extends Component {
     componentDidMount() {
         this.props.fetchStreams();
     }
+    renderList() {
+        return this.props.streams.map((stream) => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        <div className="header">{stream.title}</div>
+                        <div className="description">
+                            {stream.description}
+                            {/* <div className="right floated content">
+                                <button className="ui button secondary">
+                                    delete
+                                </button>
+                                <button className="ui button secondary">
+                                    edit
+                                </button>
+                            </div> */}
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
     render() {
-        return <div>Stream List</div>;
+        console.log("streams: ", this.props.streams);
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui middle aligned celled list">
+                    {this.renderList()}
+                </div>
+            </div>
+        );
     }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+    return {
+        streams: Object.values(state.streams) //array of streams
+    };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
