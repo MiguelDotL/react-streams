c357a3e0de545bdfd41d0c0ff7739c58c5cf8fcb
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-10 20:15:06 -0400

    generalize form from StreamCreate for reusability

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
 
-const StreamEdit = () => {
-    return <div>Stream Edit</div>;
+class StreamEdit extends Component {
+    componentDidMount() {
+        const streamId = this.props.match.params.id;
+
+        this.props.fetchStream(streamId);
+    }
+
+    render() {
+        const stream = this.props.stream;
+
+        if (!stream) {
+            return (
+                <div className="ui active inverted dimmer">
+                    <div className="ui large text loader">Loading</div>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <h2>Edit Stream</h2>
+                <div>Title: {stream.title}</div>
+                <div>Description: {stream.description}</div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const streamId = ownProps.match.params.id;
+
+    return {
+        stream: state.streams[streamId]
+    };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream, editStream })(
+    StreamEdit
+);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,72 @@
+import { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends Component {
+    renderError({ touched, error }) {
+        if (touched && error) {
+            return (
+                <div className="ui small warning message">
+                    <div className="header">{error.head}</div>
+                    <div className="">{error.body}</div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = ({ input, label, meta }) => {
+        const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
+            </div>
+        );
+    };
+
+    onSubmit = (formValues) => {
+        // console.log("formValues: ", formValues);
+        this.props.onSubmit(formValues);
+    };
+
+    render() {
+        console.log("props: ", this.props);
+        return (
+            <form
+                className="ui form warning"
+                onSubmit={this.props.handleSubmit(this.onSubmit)}
+            >
+                <Field
+                    name="title"
+                    label="Stream Title"
+                    component={this.renderInput}
+                />
+                <Field
+                    name="description"
+                    label="Stream Description"
+                    component={this.renderInput}
+                />
+                <button className="ui primary button">Submit</button>
+            </form>
+        );
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = {
+            head: " 🐣 Wait!",
+            body: " Your stream needs a!"
+        };
+    }
+    if (!formValues.description) {
+        errors.description = {
+            head: "🐥 Quackers!",
+            body: "Don't forget to add a description to your stream to help others find you!"
+        };
+    }
+    return errors;
+};
+
+export default reduxForm({ form: "streamForm", validate })(StreamForm);
